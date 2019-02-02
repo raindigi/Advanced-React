6e5f85da9facf4e3c920ba25e23e438df37ab142
Brittany Chiang <brittany.chiang@gmail.com>
2019-02-02 16:06:42 -0500

    Module 2

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,11 +1,24 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-  <div>
+  <NavStyles>
+    <Link href="/items">
+      <a href="/items">Items</a>
+    </Link>
     <Link href="/sell">
       <a href="/sell">Sell</a>
     </Link>
-  </div>
+    <Link href="/signup">
+      <a href="/signup">Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a href="/orders">Orders</a>
+    </Link>
+    <Link href="/me">
+      <a href="/me">Account</a>
+    </Link>
+  </NavStyles>
 );
 
 export default Nav;
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,54 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightgrey: '#e1e1e1',
+  offWhite: '#ededed',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0.09)',
+};
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends Component {
   static propTypes = {
@@ -10,11 +57,14 @@ class Page extends Component {
 
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <GlobalStyle />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
